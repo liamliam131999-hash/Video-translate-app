9c0ad188e1ebdeab811af95df360365c4dcd7dad
liamliam131999-hash <liamliam.131999@gmail.com>
2026-09-25 18:00:55 +0630

    Create MainActivity.java

---
+++ app/src/main/java/com/app/videotranslate/MainActivity.java
@@ -0,0 +1,36 @@
+package com.app.videotranslate;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        
+        webView.setWebViewClient(new WebViewClient());
+        
+        // သင့်ရဲ့ Streamlit App လင့်ခ်အမှန်
+        webView.loadUrl("https://video-translate-app-dussitpq5z6ytsi5ghfhd5.streamlit.app/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
